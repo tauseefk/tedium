9b8b6308b071214011aec798d80dad3043d71330
tauseefk <tauseef25@gmail.com>
2023-02-28 09:00:27 +0000

    Deploying to gh-pages from  @ c786ad0e4072519a360cc8ffc776b69ba98dcb9b 🚀

--- src/events.rs
+++
@@ -1,9 +0,0 @@
-use crate::prelude::*;
-
-// Add or remove a wall block
-pub struct ToggleWallBlockEvent {
-    pub translation: Vec3,
-}
-
-// Event to cycle through Points of Interest
-pub struct CyclePOIEvent;
