2439056beb5f4ccc56708ba5d9558144df3b2891
tauseefk <tauseef25@gmail.com>
2024-12-01 22:07:02 -0800

    trying to fix the map rendering

--- src/systems/setup.rs
+++ src/systems/setup.rs
@@ -6,9 +6,22 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     commands.spawn(camera);
 
-    let ldtk_handle = asset_server.load("basic_map.ldtk");
+    let ldtk_handle = asset_server.load("maps/basic_map.ldtk");
     commands.spawn(LdtkWorldBundle {
         ldtk_handle,
+        ..Default::default()
+    });
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::linear_rgb(0., 0., 255.0),
+            custom_size: Some(Vec2::new(64.0, 64.0)),
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0., 0., 1.),
+            ..Default::default()
+        },
         ..Default::default()
     });
 }
